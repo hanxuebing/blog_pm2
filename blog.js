7f3c9475b7df57ee2e4cb6da95dd9d3cb6960d08
hanxuebing <antneststudio@126.com>
2021-12-14 18:54:58 +0800

    fix:修复配置路径

--- blog.js
+++ blog.js
@@ -4,10 +4,11 @@ const path = require('path');
 const express = require('express');
 const chalk = require('chalk')
 const blog = express();
-blog.use(express.static(path.resolve(__dirname, '../blog')))
+const baseDir = __dirname + '../blog'; 
+blog.use(express.static(path.resolve(baseDir)))
  
 blog.get('*', function(req, res) {
-    const html = fs.readFileSync(path.resolve(__dirname, '../blog/index.html'), 'utf-8')
+    const html = fs.readFileSync(path.resolve(baseDir, '/index.html'), 'utf-8')
     res.send(html)
 })
 blog.listen(8080, res => {
